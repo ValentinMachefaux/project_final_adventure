9e26dcf6a4c0050a89d30f791f1121f4aa24e334
Valentin Machefaux <valentin.machefaux@gmail.com>
2023-01-09 17:45:03 +0100

    -add d'autres trucs

--- Heros.py
+++ Heros.py
@@ -5,4 +5,5 @@ class Heros(Character):
     def __init__(self):
         super().__init__(self)
 
-    # TO DO : recup les mouvements, get l'inventaire et autres trucs
+    
+    # TO DO : recup les mouvements, get le menu, get les interactions
